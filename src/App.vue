8bd69ef79ddd73c2d823e263a85c458f23c8dade
samuel <samuel.li2@student.tdsb.on.ca>
2024-11-22 13:46:39 -0500

    added some html and css

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,78 @@
 </template>
 
 <style scoped>
+body{
+  background-color: aqua;
+}
+.header {
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+background: rgba(0, 0, 0, 0.7);
+padding: 10px 20px;
+z-index: 100;
+}
+h1 {
+font-family: "Pacifico", cursive;
+text-align: center;
+margin-top: 20px;
+}
+h2 {
+text-align: left;
+margin: 20px 0 10px 0;
+}
 
+h3 {
+text-align: center;
+color: gold;
+margin: 10px 0;
+}
+
+p {
+color: black;
+text-align: center;
+margin: 10px 0;
+}
+
+.movies {
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+gap: 2%;
+margin: 100px 0 20px;
+}
+.movies img {
+border: 3px solid red;
+width: 200px;
+height: auto;
+object-fit: cover;
+transition: opacity 0.3s ease, transform 0.3s ease;
+}
+.movies img:hover {
+opacity: 0.8;
+
+transform: scale(1.05);
+}
+.button-container {
+text-align: center;
+margin: 0;
+}
+.language-btn {
+margin-right: 10px;
+padding: 10px 20px;
+background-color: #ffcc00;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+font-weight: bold;
+transition: background-color 0.3s;
+}
+.language-btn:hover {
+background-color: #ff9900;
+}
+footer {
+text-align: center;
+margin: 20px 0;
+z-index: 2;
 </style>
---
+++ src/Feature.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Superhero Movies</h1>
+    <h3>This is a superhero movie catalog. In this website, you can find all
+    movies related to superhero films. Movies such Superman, Batman and more can be
+    found in this website</h3>
+
+    <div>
+        <button class="language-btn">English</button>
+        <button>Sign In</button>
+        <button>Sign Out</button>
+    </div>
+    <h2>Top Rated Movies</h2>
+    <div class="movies">
+        <img src="deadpool_.png" alt="Poster 1" width="200" height="200">
+        <img src="batman2.0.png" alt="Poster 2" width="200" height="200">
+        <img src="spiderman.png" alt="Poster 3" width="200" height="200">
+        <img src="acoss the verse.png" alt="Poster 4" width="200" height="200">
+        <img src="flash.png" alt="Poster 5" width="200" height="200">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
