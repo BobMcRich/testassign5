e05fde11b42f07532301774f15005acdc2508460
Samuel Li <samuelforlife13@gmail.com>
2024-11-24 23:07:27 -0500

    starting homeview

---
+++ src/Views/HomeView.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HeroFilms</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" href="superhero icon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <style>
+        h1 {
+            font-family: "Pacifico", cursive;
+        }
+    </style>
+</head>
+<body>
+    <h1>Superhero Movies</h1>
+    <h3>This is a superhero movie catalog. In this website, you can find all movies related to superhero films. Movies such Superman, Batman and more can be found in this website</h3>
+    
+    
+    <div>
+        <button class="language-btn">English</button>
+        <button>Sign In</button>
+        <button>Sign Out</button>
+    </div>
+
+    <h2>Top Rated Movies</h2>
+    <div class="movies">
+        <img src="deadpool_.png" alt="Poster 1" width="200" height="200">
+        <img src="batman2.0.png" alt="Poster 2" width="200" height="200">
+        <img src="spiderman.png" alt="Poster 3" width="200" height="200">
+        <img src="across_the_verse.png" alt="Poster 4" width="200" height="200">
+        <img src="flash.png" alt="Poster 5" width="200" height="200">
+    </div>
+    
+    <footer>
+        <p>HeroFilms, 2024</p>
+    </footer>
+</body>
+</html>
+
+</template>
+
+<style scoped>
+
+</style>
